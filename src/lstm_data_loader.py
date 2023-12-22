467a44cb88650a7278e6ad6a844f889b68ad5667
arsenaultk9 <arsenaultk9@gmail.com>
2023-12-22 11:46:11 -0500

    lstm skip example with short content.

--- src/lstm_data_loader.py
+++ src/lstm_data_loader.py
@@ -1,6 +1,7 @@
 import pickle
 import torch
 
+import src.constants as constants
 from src.models.lstm_dataset import LstmSequenceDataset
     
 def get_inputs_tensor(data):
@@ -8,12 +9,20 @@ def get_inputs_tensor(data):
     return inputs
 
 def get_data_set(data):
-    inputs = get_inputs_tensor(data)
+    inputs = data['input_ids']
+    attention_masks = data['attention_mask']
     
     Xs = []
     Ys = []
     
-    for input in inputs:
+    for index, input in enumerate(inputs):
+        attention_mask = attention_masks[index]
+        mask_stops = [i for i, j in enumerate(attention_mask) if j == 0]
+        
+        # Skip short sequences
+        if len(mask_stops) > 0 and mask_stops[0] < (constants.SEQUENCE_LENGTH - 10):
+            continue
+        
         x = input[0:-1]
         y = input[1:]
         
